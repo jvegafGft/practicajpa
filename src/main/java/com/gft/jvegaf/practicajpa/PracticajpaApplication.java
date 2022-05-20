953c5ad1f617438a3ef1868d34ec7cde33bde1ce
Jose Vega <Jose.Vega@gft.com>
2022-05-21 01:16:41 +0200

    Project boilerplate
    Signed-off-by: Jose Vega <Jose.Vega@gft.com>

---
+++ src/main/java/com/gft/jvegaf/practicajpa/PracticajpaApplication.java
@@ -0,0 +1,13 @@
+package com.gft.jvegaf.practicajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticajpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticajpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gft/jvegaf/practicajpa/PracticajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.jvegaf.practicajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticajpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
